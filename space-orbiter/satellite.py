3968210084751ecea4228fcf4714bc8de9600ee6
dan-bowman <dbowman5@live.com>
2020-07-21 15:40:09 -0400

    move files

--- space-orbiter/satellite.py
+++
@@ -1,7 +0,0 @@
-# This class is identical to the Mobile class but will remain so that it can be extended later
-from mobile import Mobile
-
-
-class Satellite(Mobile):
-    def __init__(self, spawn, img_path, size, mass, influence_height, vel_vector):
-        super().__init__(spawn, img_path, size, mass, influence_height, vel_vector)
